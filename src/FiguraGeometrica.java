613bd017c2627240e74666f339b6068841ccd432
julibarra <julieta.ibarra@mercadolibre.com>
2021-02-24 11:44:56 -0300

    Ejercicios de la practica TT

--- src/FiguraGeometrica.java
+++ src/FiguraGeometrica.java
@@ -2,7 +2,16 @@ public abstract class FiguraGeometrica {
     public abstract double area();
 
     public void imprimirArea(){
+
         System.out.println("Area de la figura: "+ area());
+    }
+
+    public static double areaPromedio(FiguraGeometrica array[]){
+        double suma=0;
+        for (FiguraGeometrica figura:array) {
+            suma+=figura.area();
+        }
+        return suma/array.length;
     }
 
 
--- src/Password.java
+++ src/Password.java
@@ -1,2 +1,46 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Password {
+   private String formato;
+    private Pattern pattern;
+   // private Matcher matcher;
+
+
+    public Password(String formato) {
+        this.formato = formato;
+        this.pattern= Pattern.compile(formato);
+
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public Pattern getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String formato) {
+        this.pattern= Pattern.compile(formato);
+    }
+/*
+    public Matcher getMatcher() {
+        return matcher;
+    }
+
+    public void setMatcher(String psswr) {
+        this.matcher = pattern.matcher(psswr);
+    }
+
+ */
+    public void setValue(String pssw){
+       // Matcher matcher=pattern.matcher();
+        if (!pattern.matches(getFormato(),pssw))
+            throw new RuntimeException("El deniminador tiene que mayor al numerador");
+}
 }
